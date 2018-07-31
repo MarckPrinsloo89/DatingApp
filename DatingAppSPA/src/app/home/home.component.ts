a84eb48c88a619a229bac6801b2dc491688aaf21
Marck Prinsloo <marck.prinsloo@gmail.com>
2018-08-01 01:39:21 +0200

    End of Section 5

--- DatingAppSPA/src/app/home/home.component.ts
+++ DatingAppSPA/src/app/home/home.component.ts
@@ -16,8 +16,8 @@ export class HomeComponent implements OnInit {
   }
 
   registerToggel() {
-    this.registerMode = true;
-  }
+    this.registerMode = true;  }
+
 
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
